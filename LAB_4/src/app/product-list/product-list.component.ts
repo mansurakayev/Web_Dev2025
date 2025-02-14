85728203730653c93a2dc9c499f50dddfe4d5764
Anel Adalbek <aneladalbek@MacBook-Pro-Anel-2.local>
2025-02-14 19:32:25 +0300

    lab4 files commit

---
+++ LAB_4/src/app/product-list/product-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Product, products } from '../products';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css'],
+})
+export class ProductListComponent {
+  products: Product[] = products;
+  searchInput: string = '';
+
+  get filteredProducts(): Product[] {
+    const searchTerm = this.searchInput.toLowerCase();
+    return this.products.filter(product => product.name.toLowerCase().includes(searchTerm));
+  }
+
+  sharetg(productName: string, productLink: string) {
+    const shareMessage = `Check out this product: ${productName} - ${productLink}`;
+    const telegramLink = `https://t.me/share/url?url=${encodeURIComponent(shareMessage)}`;
+    window.location.href = telegramLink;
+  }
+
+}
